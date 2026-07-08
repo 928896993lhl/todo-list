c88bdca24590ac8ac55a835baca429faedad8782
928896993lhl <928896993lhl@users.noreply.github.com>
2026-07-09 01:51:56 +0800

    fix: 修正云开发环境ID为dev-allknow-3gni7da4dc75550b

--- miniprogram/app.js
+++ miniprogram/app.js
@@ -4,7 +4,7 @@ App({
       console.error('请使用 2.2.3 或以上的基础库以使用云能力')
     } else {
       wx.cloud.init({
-        env: 'dev-0ggqs5te8ed2bcdd',
+        env: 'dev-allknow-3gni7da4dc75550b',
         traceUser: true
       })
     }
