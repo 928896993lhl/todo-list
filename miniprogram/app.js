4247f6be0b4ae2b47f825e19e82ac343623d5eaf
928896993lhl <928896993lhl@users.noreply.github.com>
2026-07-09 01:45:45 +0800

    fix: 修正云开发环境ID为dev-0ggqs5te8ed2bcdd

--- miniprogram/app.js
+++ miniprogram/app.js
@@ -4,7 +4,7 @@ App({
       console.error('请使用 2.2.3 或以上的基础库以使用云能力')
     } else {
       wx.cloud.init({
-        env: 'openclaw01-d4gl0o8p871b0efbe',
+        env: 'dev-0ggqs5te8ed2bcdd',
         traceUser: true
       })
     }
